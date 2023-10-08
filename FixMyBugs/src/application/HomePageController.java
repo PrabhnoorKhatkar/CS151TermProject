bb2203d917025a044dd13c5aa2fa706b85ada892
PrabhnoorKhatkar <prabhnoorkhatkar@gmail.com>
2023-10-07 17:27:51 -0700

    Code 0.2 Submission

--- FixMyBugs/src/application/HomePageController.java
+++ FixMyBugs/src/application/HomePageController.java
@@ -19,16 +19,28 @@ public class HomePageController
 	
 	private Stage HomepageStage;
 	
-	
+	/**
+	 * This method handles the action event when a user wants to go to the new project scene.
+	 * It loads the "NewProjectPage.fxml" file, creates a new scene, and sets it as the current scene.
+	 * It also updates the stage to display the new scene.
+	 *
+	 * @param event The ActionEvent triggered by the user's interaction.
+	 * @throws IOException If there is an error loading the FXML file.
+	 */
 	
 	public void goToNewProjectScene(ActionEvent event) throws IOException 
 	{
-		Parent root = FXMLLoader.load(getClass().getResource("NewProjectPage.fxml")); // Set the style and properties of the page into parent root
+		// Set the style and properties of the page into parent root
+		Parent root = FXMLLoader.load(getClass().getResource("NewProjectPage.fxml")); 
 		Node source = (Node) event.getSource();
-	    HomepageStage = (Stage) source.getScene().getWindow(); // Get the Stage from which the button was pressed in
-		Scene newProjectScene = new Scene(root); // Create the scene from the "NewProjectPage.fxml"
-		HomepageStage.setScene(newProjectScene); // Override the old scene and replace with new one
-		HomepageStage.show(); // Update the scene
+		// Get the Stage from which the button was pressed in
+	    HomepageStage = (Stage) source.getScene().getWindow();
+	    // Create the scene from the "NewProjectPage.fxml"
+		Scene newProjectScene = new Scene(root); 
+		// Override the old scene and replace with new one
+		HomepageStage.setScene(newProjectScene); 
+		// Update the scene
+		HomepageStage.show(); 
 	
 	}
 
--- FixMyBugs/src/application/Main.java
+++ FixMyBugs/src/application/Main.java
@@ -9,19 +9,36 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application 
 {
+	
+	/**
+	 * This method initializes the primary stage and sets up the initial scene.
+	 *
+	 * @param primaryStage The primary stage where the application's UI is displayed.
+	 */
+	
 	@Override
 	public void start(Stage primaryStage) 
 	{
 		try {
+			// Load the "HomePage.fxml" file and create a root Pane from it
 			Pane root = (Pane)FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+			// Create a new scene with the root Pane and set its dimensions (400x400)
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// Set the created scene as the scene for the primary stage
 			primaryStage.setScene(scene);
+			// Show the primary stage to display the UI
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * The main method is meant for launching the Java application.
+	 * It calls the launch method to start the JavaFX application.
+	 *
+	 * @param args Command-line arguments (not used in this application).
+	 */
 	
 	public static void main(String[] args) {
 		launch(args);
