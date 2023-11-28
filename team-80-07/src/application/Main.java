2b6dcfd55cc52f2a614657aef06f9a557dc46826
Rimon Yacoub <rimonyacoub@Rimons-iMac.local>
2023-11-28 13:48:29 -0800

    spalshscreen

--- team-80-07/src/application/Main.java
+++ team-80-07/src/application/Main.java
@@ -1,47 +1,124 @@
 package application;
-	
+
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.SequentialTransition;
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+/**
+ * The main class responsible for launching the JavaFX application.
+ */
+public class Main extends Application {
 
-public class Main extends Application 
-{
-	
-	/**
-	 * This method initializes the primary stage and sets up the initial scene.
-	 *
-	 * @param primaryStage The primary stage where the application's UI is displayed.
-	 */
-	
-	@Override
-	public void start(Stage primaryStage) 
-	{
-		try {
-			// Load the "HomePage.fxml" file and create a root Pane from it
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-			// Create a new scene with the root Pane and set its dimensions (400x400)
-			Scene scene = new Scene(root,400,400);
-			// Set the created scene as the scene for the primary stage
-			primaryStage.setScene(scene);
-			// Show the primary stage to display the UI
-			primaryStage.show();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * This method is meant for launching the Java application.
-	 * It calls the launch method to start the JavaFX application.
-	 *
-	 * @param args Command-line arguments (not used in this application).
-	 */
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    private Stage primaryStage;
+    private Parent root; // Declare root as a class variable
+
+    /**
+     * The main entry point for the JavaFX application.
+     *
+     * @param primaryStage The primary stage for the application.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        splashScreenLoader();
+    }
+
+    /**
+     * Loads and displays the splash screen with fade-in and fade-out transitions.
+     */
+    private void splashScreenLoader() {
+        try {
+            root = FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
+            Scene splashScene = new Scene(root);
+
+            // Setting up the fade-in transition for the splash screen
+            FadeTransition fadeIn = new FadeTransition(Duration.seconds(5), root);
+            fadeIn.setFromValue(0);
+            fadeIn.setToValue(1);
+
+            // Setting up the fade-out transition for the splash screen
+            FadeTransition fadeOut = new FadeTransition(Duration.seconds(5), root);
+            fadeOut.setFromValue(1);
+            fadeOut.setToValue(0);
+
+            // Combining both transitions into a sequential transition
+            SequentialTransition sequence = new SequentialTransition(fadeIn, fadeOut);
+            sequence.setOnFinished(e -> loadMainPage());
+
+            primaryStage.setScene(splashScene);
+            primaryStage.show();
+
+            // Starting the sequential transition
+            sequence.play();
+            
+            // Add image rotation after a delay using Platform.runLater
+            Platform.runLater(() -> {
+                ImageView imageView = (ImageView) root.lookup("#SplashImage2"); // Replace with your image view ID
+                if (imageView != null) {
+                    rotateImage(imageView);
+                }
+            });
+            // Add image rotation after a delay using Platform.runLater
+            Platform.runLater(() -> {
+                ImageView imageView = (ImageView) root.lookup("#SplashImage1"); // Replace with your image view ID
+                if (imageView != null) {
+                    rotateImage(imageView);
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads and displays the main page with image rotation after a delay.
+     */
+    private void loadMainPage() {
+        try {
+            root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+            Scene mainScene = new Scene(root, 600, 400);
+            primaryStage.setScene(mainScene);
+
+            // Add image rotation after a delay using Platform.runLater
+            Platform.runLater(() -> {
+                ImageView imageView = (ImageView) root.lookup("#projectImageView"); // Replace with your image view ID
+                if (imageView != null) {
+                    rotateImage(imageView);
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Applies a rotation animation to the specified ImageView.
+     *
+     * @param imageView The ImageView to be rotated.
+     */
+    private void rotateImage(ImageView imageView) {
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(3), imageView);
+        rotateTransition.setByAngle(360); // Rotate by 360 degrees
+        rotateTransition.setCycleCount(RotateTransition.INDEFINITE); // Repeat indefinitely
+        rotateTransition.play();
+    }
+
+    /**
+     * The main method to launch the JavaFX application.
+     *
+     * @param args Command-line arguments (not used in this application).
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
